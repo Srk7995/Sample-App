65f065c4eb33a849536d84a9b4642755cbaba5e2
Shahrukh <shahrukhkhann7995@gmail.com>
2022-12-06 09:31:43 +0000

    Susbtract Prog

--- calc.py
+++ calc.py
@@ -1,3 +1,11 @@
-Calculator Program 
+Calculator Program
 
 
+Substraction Program
+
+
+num1 = int(input("Please Enter the First Number  = "))
+num2 = int(input("Please Enter the second number = "))
+
+sub = num1 - num2
+print('The Result of subtracting {0} from {1} = {2}'.format(num2,num1,sub))
