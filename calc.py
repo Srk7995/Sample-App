2b4d2790c2951d9f85ae58ae7eebb28e7bc6b743
Shahrukh <shahrukhkhann7995@gmail.com>
2022-12-06 09:38:28 +0000

    Divison Program

--- calc.py
+++ calc.py
@@ -1,3 +1,8 @@
 Calculator Program 
 
+Division Program
 
+number1=int(input("Enter the first number: "))
+  number2=int(input("Enter the second number: "))
+  result=number1/number2;
+  print(result)
